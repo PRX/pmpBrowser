fc755474049c3f83ebd188eb4894750cd64a3f53
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-09-23 09:20:19 -0400

    change services to pmp

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('pmpBrowser', ['ionic', 'pmpBrowser.controllers', 'pmpBrowser.services'])
+angular.module('pmpBrowser', ['ionic', 'pmpBrowser.controllers', 'pmp'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -63,6 +63,6 @@ angular.module('pmpBrowser', ['ionic', 'pmpBrowser.controllers', 'pmpBrowser.ser
       }
     });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/recommended');
+  $urlRouterProvider.otherwise('/app/search');
 });
 
