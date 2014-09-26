196d4a3da5a1dc9b2c769e9b584cb353ed1f78e4
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-09-26 11:41:08 -0400

    metadata and cleanup querying

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,51 +2,70 @@ angular.module('pmpBrowser.controllers', [])
 .controller('DocCtrl', function ($scope, doc) {
   $scope.doc = doc;
 })
-.controller('AppCtrl', function ($scope, $pmp, $ionicLoading, CollectionDoc) {
-  $scope.search = {};
+.controller('AppCtrl', function ($scope, $pmp, $ionicLoading, CollectionDoc, metadata) {
+
   $scope.items = [];
 
+  $scope.search = {};
+
+  $scope.orgFor = function (doc) {
+    return metadata.orgInfo(doc.links.creator[0].href);
+  }
+
   $scope.$watch('search.string', function (newValue, oldValue) {
-    debouncedSearch();
+    debouncedSearch(newValue, oldValue);
   });
 
-  var debouncedSearch = ionic.debounce(function () {
+  var debouncedSearch = ionic.debounce(function (newValue, oldValue) {
     $scope.$apply(function (){
-      $scope.items = [];
-      $scope.pmpSearch($scope.search.string);
+      if ((newValue || '') != (oldValue || '')) {
+        $scope.search.result = null;
+        $scope.items = [];
+        $scope.pmpSearch($scope.search.string);
+      }
     });
   }, 400, false);
 
-
   $scope.moreDataCanBeLoaded = function () {
-    var loadMore = !!($scope.search.result && $scope.search.result.hasLink('next'));
+    var loadMore = false;
+
+    if (!$scope.search || !$scope.search.result) {
+      loadMore = true;
+    } else {
+      loadMore = $scope.search.result.hasLink('next');
+    }
     return loadMore;
   };
 
   $scope.loadMore = function () {
-    $scope.search.result.next().then(
-      function (doc) {
-        if (doc) {
-          $scope.search.result = doc;
-          $scope.items = $scope.items.concat(doc.items);
-          $scope.$broadcast('scroll.infiniteScrollComplete');
+    if ($scope.search && $scope.search.result) {
+      console.log('loadMore, result next');
+      $scope.search.result.next().then(
+        function (doc) {
+          if (doc) {
+            $scope.search.result = doc;
+            $scope.items = $scope.items.concat(doc.items);
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+          }
         }
-      }
-    );
+      );
+    } else {
+      console.log('loadMore, no result', $scope.search.string);
+      $scope.pmpSearch($scope.search.string).then( function() {
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
+    }
   };
 
   $scope.pmpSearch = function (query, options) {
-    // // use this to prevent search unless there is text
-    // if (angular.isUndefined(query) || query.length <= 2) {
-    //   $scope.search.result = null;
-    //   return $scope.search.result;
-    // }
+    $ionicLoading.show({ template: '<i class="icon ion-loading-d icon-refreshing"></i> Loading...', noBackdrop: true });
+    var vars = options || {};
+    if (query) {
+      vars['text'] = query;
+    }
 
-    $ionicLoading.show({ template: '<i class="icon ion-loading-d"></i> Loading...', noBackdrop: true });
-
-    CollectionDoc.search({text: query}).then(
+    return CollectionDoc.search(vars).then(
       function (doc) {
-        // console.log('pmp doc', doc, doc.items);
         $scope.search.result = doc;
         $scope.items = $scope.items.concat(doc.items);
         $ionicLoading.hide();
@@ -57,4 +76,5 @@ angular.module('pmpBrowser.controllers', [])
       }
     );
   };
+
 });
